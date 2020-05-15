c7e3f04c3f3aca1e2308638086d2637dad23ea95
Fernando Prieto Moyano <fernando.moyano@nice.agency>
2020-05-15 08:01:49 +0100

    Rest of features work

--- features/main/src/main/java/prieto/fernando/main/di/MainActivityModule.kt
+++ features/main/src/main/java/prieto/fernando/main/di/MainActivityModule.kt
@@ -3,13 +3,19 @@ package prieto.fernando.main.di
 import dagger.Module
 import dagger.Provides
 import dagger.android.ContributesAndroidInjector
+import prieto.fernando.custom.di.CustomModule
 import prieto.fernando.dashboard.di.DashboardModule
+import prieto.fernando.detail.di.DetailModule
+import prieto.fernando.infinite.di.InfiniteModule
 import prieto.fernando.main.vm.MainViewModel
 import prieto.fernando.vm.ViewModelProviderFactory
 
 @Module(
     includes = [
-        DashboardModule::class
+        DashboardModule::class,
+        DetailModule::class,
+        CustomModule::class,
+        InfiniteModule::class
     ]
 )
 abstract class MainActivityModule {
